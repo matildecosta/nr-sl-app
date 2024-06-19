beb4c04a38ce4efbd60ed401bdbb91d5770f254f
matilde <matilde.a.costa@av.it.pt>
2024-06-19 11:28:35 +0100

    update running scripts

---
+++ rfsim_sh/runUE1_rfsim.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ../openairinterface5g/cmake_targets/ran_build/build
+sudo ./nr-uesoftmodem --rfsim -O ~/OAI_NR_SL/nr-sl-app/sl_ue1.conf --sl-mode 2 --sa --rfsimulator.serveraddr 192.168.81.217 --rfsimulator.serverport 4040
--- rfsim_sh/runUE1_rfsim_pramheda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ~/OAI_NR_SL/openairinterface5g/cmake_targets/ran_build/build
-sudo ./nr-uesoftmodem --rfsim -O ~/OAI_NR_SL/nr-sl-app/sl_ue1.conf --sl-mode 2 --sa --rfsimulator.serveraddr 172.21.19.73 --rfsimulator.serverport 4048

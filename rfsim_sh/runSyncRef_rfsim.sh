102fb6782186dc7d3411a51eca3c5e7ddd8c7725
matilde <matilde.a.costa@av.it.pt>
2024-06-19 11:34:44 +0100

    update scripts

--- rfsim_sh/runSyncRef_rfsim.sh
+++ rfsim_sh/runSyncRef_rfsim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../openairinterface5g/cmake_targets/ran_build/build
-sudo ./nr-uesoftmodem --rfsim -O ~/OAI_NR_SL/nr-sl-app/sl_syncref.conf --sl-mode 2 --sa --sync-ref --rfsimulator.serveraddr server --rfsimulator.serverport 4048
+sudo ./nr-uesoftmodem --rfsim -O ~/nr-sl-app/sl_syncref.conf --sl-mode 2 --sa --sync-ref --rfsimulator.serveraddr server --rfsimulator.serverport 4048

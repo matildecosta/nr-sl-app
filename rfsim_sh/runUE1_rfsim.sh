a8347ba92b87b2f8e6c29557ba5b3e1fe016aa47
Matilde Costa <matilde.a.costa@av.it.pt>
2024-07-23 15:53:10 +0100

    python code for ping

--- rfsim_sh/runUE1_rfsim.sh
+++ rfsim_sh/runUE1_rfsim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../openairinterface5g/cmake_targets/ran_build/build
-sudo ./nr-uesoftmodem --rfsim -O ~/nr-sl-app/sl_ue1.conf --sl-mode 2 --sa --rfsimulator.serveraddr 192.168.81.217 --rfsimulator.serverport 4048
+sudo ./nr-uesoftmodem --rfsim -O ~/nr-sl-app/sl_ue1.conf --sl-mode 2 --sa --rfsimulator.serveraddr 192.168.81.235 --rfsimulator.serverport 4048

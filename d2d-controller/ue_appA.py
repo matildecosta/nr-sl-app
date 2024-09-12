8cda726b3df2d5fde48b59f0bee2bed02748b33a
Matilde Costa <matilde.a.costa@av.it.pt>
2024-09-12 11:08:14 +0100

    Update ue apps, both tcp and udp work

--- d2d-controller/ue_appA.py
+++ d2d-controller/ue_appA.py
@@ -3,11 +3,13 @@ import threading
 import time
 
 location = "36.9482,-25.0191"
-user_b_ip = '10.0.0.2'
+p2p_ip = '10.0.0.2'
 broadcast_ip = '10.0.0.255'
-user_b_port = 50001
-self_port = 50002
-interface_name = 'oaitun_ue1'
+self_ip = '10.0.0.1'
+p2p_port = 50001    # port of the listening socket on the other user
+self_port = 50002   # port of the listening socket on this user
+source_port = 50001 # port of the sending socket
+interface_name = "oaitun_ue1"
 
 user = "A"
 time_interval = 1  # Send every 1 second
@@ -15,79 +17,82 @@ time_interval = 1  # Send every 1 second
 type="TCP"
 
 def send_packet():
-    print("Send packet thread started")
-
-    if type == "TCP":
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, interface_name.encode())
-    else:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, interface_name.encode())
+    print("[S] Send packet thread started")
     
     if type == "TCP":
-        try:
-            # TCP
-            sock.connect((user_b_ip, user_b_port))
-            print(f"Connected to {user_b_ip}:{user_b_port}")
+        while True:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.bind((self_ip, source_port))
+                sock.connect((p2p_ip, p2p_port))
+                print(f"[S] Connected to {p2p_ip}:{p2p_port}")
 
-            while True:
-                message = user + ":" + location
-                sock.sendall(message.encode())
-                print(f"Sent: {message}")
-                time.sleep(time_interval)
+                while True:
+                    message = user + ":" + location
+                    sock.sendall(message.encode())
+                    print(f"[S] Sent: {message}")
+                    time.sleep(time_interval)
 
-        finally:
-            sock.close()
-    else:
-        # UDP
+            except Exception as e:
+                print(f"[S] Connection failed: {e}. Trying again in 2 seconds...")
+                time.sleep(2)
+            finally:
+                sock.close()
+
+
+    else:   # UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # To allow broadcasting
+        sock.bind((self_ip, source_port))
+
         while True:
             try:
                 message = user + ":" + location
-                sock.sendto(message.encode(), (broadcast_ip, user_b_port))
-                print(f"Sent: {message}")
+                sock.sendto(message.encode(), (broadcast_ip, p2p_port))
+                # sock.sendto(message.encode(), (p2p_ip, p2p_port)) # To send individually
+                print(f"[S] Sent: {message}")
                 time.sleep(time_interval)
             except Exception as e:
-                print(f"Error sending packet: {e}")
+                print(f"[S] Error sending packet: {e}")
+            
 
 def listening_socket():
-    print("Listening socket thread started")
+    print("[L] Listening socket thread started")
 
     if type == "TCP":
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, interface_name.encode())
-        server_socket.bind(('10.0.0.1',self_port))
+        server_socket.bind((self_ip,self_port))
         server_socket.listen(1)
-        print("User", user, "is listening for connections on port", self_port)
+        print("[L] User", user, "is listening for connections on port", self_port)
 
         while True:
-            print("Waiting for a connection...")
+            print("[L] Waiting for a connection...")
             connection, client_address = server_socket.accept()
-            print(f"Connection from {client_address}")
+            print(f"[L] Connection from {client_address}")
 
             try:
                 while True:
                     # Receive the data in chunks
                     data = connection.recv(1024)
                     if data:
-                        print(f"Received: {data.decode()}")
+                        print(f"[L] Received: {data.decode()}")
                     else:
                         break
             finally:
-                print(f"Closing connection with {client_address}")
+                print(f"[L] Closing connection with {client_address}")
                 connection.close()
     else:
         # UDP
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, interface_name.encode())
         server_socket.bind(('0.0.0.0', self_port))
-        print("User", user, "is listening for connections on port", self_port)
+        print(f"[L] User {user} is listening for packets on port {self_port} and interface {interface_name}")
 
         while True:
-            print("Waiting for a packet...")
+            #print("[L] Waiting for a packet...")
             data, addr = server_socket.recvfrom(1024)
             if data:
-                print(f"Received from {addr}: {data.decode()}")
+                print(f"[L] Received from {addr}: {data.decode()}")
             else:
                 break
             
@@ -95,6 +100,8 @@ def listening_socket():
 if __name__ == "__main__":
     print("UE App starting for user", user)
     print(f"Using {type} protocol.")
+    if type == "UDP":
+        print(f"UDP allows broadcasting.")
     time.sleep(2)
 
     # Create threads for sending and listening
